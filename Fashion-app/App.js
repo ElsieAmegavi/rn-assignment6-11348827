6052971cfff26909b62f6c80fa03238310707a70
elsieroyalty <elamegavi62@gmail.com>
2024-07-02 13:06:10 +0000

    Home page: header completed

--- Fashion-app/App.js
+++ Fashion-app/App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, Image, Text } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Royalty</Text>
-      <StatusBar style="auto" />
+        <View style={styles.headerImages}>
+            <Image source={require('./assets/Menu.png')} />
+
+            <Image source={require('./assets/Logo.png')}/>
+
+            <View style={styles.innerImages}>
+            <Image 
+              source={require('./assets/Search.png')}
+            />
+            <Image 
+              style={styles.shoppingBagImage}
+              source={require('./assets/shoppingBag.png')}
+            />
+            </View>
+
+          </View>
+
+          <View style={styles.secondHeader}>
+            <Text></Text>
+          </View>
     </View>
   );
 }
@@ -13,8 +31,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#e06aeb',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff',
+    marginTop: 80,
+    paddingHorizontal: 30,
+   
   },
+  headerImages: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  innerImages: {
+    flexDirection: 'row',
+  },
+  shoppingBagImage: {
+    marginLeft: 15
+  }
 });
